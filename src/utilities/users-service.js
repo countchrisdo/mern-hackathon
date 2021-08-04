cbd75931bff3c42f81ab71dd5c16075eef5970e5
Jim Clark <jim.clark@generalassemb.ly>
2021-08-04 15:05:51 -0700

    starter code for mern-shopping cart part 1

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -2,42 +2,15 @@ import * as usersAPI from './users-api';
 
 export async function signUp(userData) {
   try {
-    // Delegate the AJAX request to the 
-    // users-api.js module which ultimately
-    // will return a JWT
+    // Delegate the network request code to the users-api.js
+    // service module which will ultimately return a JWT
     const token = await usersAPI.signUp(userData);
-    // Persist the token
+    // Persist the "token"
     localStorage.setItem('token', token);
     return getUser();
   } catch {
     throw new Error('Invalid Sign Up');
   }
-}
-
-export function getToken() {
-  // getItem returns null if there's no string
-  const token = localStorage.getItem('token');
-  if (!token) return null;
-  // Obtain the payload of the token
-  const payload = JSON.parse(atob(token.split('.')[1]));
-  if (payload.exp < Date.now() / 1000) {
-    // Token has expired - remove it from localStorage
-    // and return null
-    localStorage.removeItem('token');
-    return null;
-  }
-  return token;
-}
-
-export function getUser() {
-  const token = getToken();
-  // If there's a token, return the user object in the payload
-  // otherwise return null
-  return token ? JSON.parse(atob(token.split('.')[1])).user : null;
-}
-
-export function logOut() {
-  localStorage.removeItem('token');
 }
 
 export async function login(credentials) {
@@ -46,14 +19,35 @@ export async function login(credentials) {
     localStorage.setItem('token', token);
     return getUser();
   } catch {
-    throw new Error('Invalid Sign Up');
+    throw new Error('Bad Credentials');
   }
 }
 
+export function logOut() {
+  localStorage.removeItem('token');
+}
+
 export function checkToken() {
-  // Just so that you don't forget how to use .then
   return usersAPI.checkToken()
-    // checkToken returns a string, but let's 
-    // make it a Date object for more flexibility
     .then(dateStr => new Date(dateStr));
+}
+
+export function getToken() {
+  // getItem returns null if there's no string
+  const token = localStorage.getItem('token');
+  if (!token) return null;
+  // Check if expired, remove if it is
+  const payload = JSON.parse(atob(token.split('.')[1]));
+  // A JWT's exp is expressed in seconds, not milliseconds, so convert
+  if (payload.exp < Date.now() / 1000) {
+    localStorage.removeItem('token');
+    return null;
+  }
+  return token;
+}
+
+export function getUser() {
+  const token = getToken();
+  // If there's a token, return the user in the payload, otherwise return null
+  return token ? JSON.parse(atob(token.split('.')[1])).user : null;
 }
