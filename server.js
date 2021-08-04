d04dc3c0dd49e1c1f34bc6205d8bda542911ad75
Jim Clark <jim.clark@generalassemb.ly>
2021-08-04 10:40:24 -0700

    Fetch menu items and update state

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.use(require('./config/checkToken'));
 // Put API routes here, before the "catch all" route
 app.use('/api/users', require('./routes/api/users'));
 
+// Protect the api routes below from anonymous users
+const ensureLoggedIn = require('./config/ensureLoggedIn');
+app.use('/api/items', ensureLoggedIn, require('./routes/api/items'));
+
 // The following "catch all" route (note the *) 
 // is necessary to return the index.html on ALL
 // non-AJAX requests
--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,9 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import * as itemsAPI from '../../utilities/items-api';
 
 export default function NewOrderPage() {
   const [menuItems, setMenuItems] = useState([]);
   
+  useEffect(function() {
+    async function getItems() {
+      const items = await itemsAPI.getAll();
+      setMenuItems(items);
+    }
+    getItems();
+  }, []);  // an empty dependency array will run the effect after the first render only
+
+
   return (
+    <>
     <h1>NewOrderPage</h1>
+    <button onClick={setMenuItems}>Click to Render</button>
+    </>
   );
 }
