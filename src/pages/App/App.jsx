54f7d161d93194ca8140345d7c949368847ef3f6
Jim Clark <jim.clark@generalassemb.ly>
2021-08-04 11:33:09 -0700

    Remove NavBar import

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,7 +5,6 @@ import { getUser } from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
