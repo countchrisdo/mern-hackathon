02dd88659cbf38d98610206d4b77230808ab9b99
Jim Clark <jim.clark@generalassemb.ly>
2021-08-04 11:06:00 -0700

    Compute categories

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,17 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import * as itemsAPI from '../../utilities/items-api';
 
 export default function NewOrderPage() {
   const [menuItems, setMenuItems] = useState([]);
-  
+  const categoriesRef = useRef([]);
+
   useEffect(function() {
     async function getItems() {
       const items = await itemsAPI.getAll();
+      categoriesRef.current = items.reduce((cats, item) => {
+        const cat = item.category.name;
+        return cats.includes(cat) ? cats : [...cats, cat];
+      }, []);
       setMenuItems(items);
     }
     getItems();
   }, []);  // an empty dependency array will run the effect after the first render only
-
 
   return (
     <>
